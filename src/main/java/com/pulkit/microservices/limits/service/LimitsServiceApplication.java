b26eca6c2192b0d2f5c07996a521ad15b7431545
Pulkit Garg <pulkitgarg007@gmail.com>
2020-06-01 17:54:43 +0530

    Adding spring cloud sleuth and Spring API Gateway

--- src/main/java/com/pulkit/microservices/limits/service/LimitsServiceApplication.java
+++ src/main/java/com/pulkit/microservices/limits/service/LimitsServiceApplication.java
@@ -2,14 +2,24 @@ package com.pulkit.microservices.limits.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import brave.sampler.Sampler;
+
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan("com.pulkit.microservices.limits.*")
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
